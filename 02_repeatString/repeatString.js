b9a046bbf677970cc21a08839311a1fe5078588f
MoXcz <130593875+MoXcz@users.noreply.github.com>
2024-04-03 18:05:31 -0600

    Done first three exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    let sumOfStrings = "";
+    for (let i = 0; i < num; i++) {
+      sumOfStrings += string;
+    }
+    return sumOfStrings;
+  }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let stringToArray = string.split("");
+  let newString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += stringToArray[i];
+  }
+  return newString;
 };
 
 // Do not edit below this line
