62f498178b942e66dcbb02ed2ce87bfa28cbacb5
MoXcz <130593875+MoXcz@users.noreply.github.com>
2024-04-05 14:02:10 -0600

    Done 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array) {
+  let newArray = [];
+  let len = array.length;
+  for (let i = 0; i < len; i++) {
+    if (
+      array[i] !== arguments[1] &&
+      array[i] !== arguments[2] &&
+      array[i] !== arguments[3] &&
+      array[i] !== arguments[4]
+    ) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
