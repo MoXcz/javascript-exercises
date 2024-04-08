ae962563f7ad19b38e9911f9f088416636a1a756
MoXcz <130593875+MoXcz@users.noreply.github.com>
2024-04-07 21:40:54 -0600

    Done 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  return Number(((fahrenheit - 32) * (5 / 9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celisus) {
+  return Number(((9 / 5) * celisus + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
