501830019f2a0359bea71288fad8a19cf110aba9
MoXcz <130593875+MoXcz@users.noreply.github.com>
2024-04-07 21:17:48 -0600

    Done 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (firstNum, secondNum) {
+  let totalOfNum = 0;
+  if (
+    firstNum < 0 ||
+    secondNum < 0 ||
+    !Number.isInteger(firstNum) ||
+    !Number.isInteger(secondNum)
+  ) {
+    return "ERROR";
+  }
+  if (firstNum > secondNum) {
+    startNum = secondNum;
+    endNum = firstNum;
+  } else if (firstNum < secondNum) {
+    startNum = firstNum;
+    endNum = secondNum;
+  }
+  for (let i = startNum; i <= endNum; i++) {
+    totalOfNum += i;
+  }
+  return totalOfNum;
 };
 
 // Do not edit below this line
