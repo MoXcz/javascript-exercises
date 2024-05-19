4e2608179a218db9f56c0bda2d18cbbe9b8a537f
MoXcz <130593875+MoXcz@users.noreply.github.com>
2024-05-19 17:06:40 -0600

    Done getThetitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let booksArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        booksArr.push(arr[i].title);
+    }
+    return booksArr;
 };
 
 // Do not edit below this line
