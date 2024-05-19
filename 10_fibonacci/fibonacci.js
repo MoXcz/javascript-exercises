c63060425c57b22dd2c253f5cdca714eee96406a
MoXcz <130593875+MoXcz@users.noreply.github.com>
2024-05-18 22:27:17 -0600

    Done fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    let fib = [0];
+    let num1 = 0;
+    let num2 = 1;
+    let num3 = 0;
+    for (let i = 0; i < 10; i++) {
+        num3 = num1+num2;
+        num2 = num3+num1;
+        num1 = num3+num2;
+        fib.push(num3, num2, num1);
+    }
+    return fib[num];
 };
 
 // Do not edit below this line
