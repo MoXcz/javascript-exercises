edb7a3fed2798e69665a17fd44c92dda1ed3b154
MoXcz <130593875+MoXcz@users.noreply.github.com>
2024-05-18 22:26:59 -0600

    Done palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+    word = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+        .toLowerCase()
+        .split(" ")
+        .join("");
+    newWord = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+        .toLowerCase()
+        // Divide the string's characters into an indexed array, reverse
+        // its order and join it into an array
+        .split("").
+        reverse().
+        join("").
+        // To divide the string spaces into an indexed array and join it
+        // into the final array
+        split(" ").
+        join("");
+    if (newWord === word) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = palindromes;
