7f20c7e6ed85e8e7d93949700e439c4bef0e46a9
MoXcz <130593875+MoXcz@users.noreply.github.com>
2024-05-20 14:28:29 -0600

    Done findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    // Assigns as 'year of death' the current date, this helps know who the
+    // oldest person is
+    for (let i = 0; i < arr.length; i++) {
+        if (!arr[i].yearOfDeath) {
+            arr[i].yearOfDeath = new Date().getFullYear();
+        }
+    }
 
+    let newArr = [];
+    // The array saves all the ages of the persons by substracting their year
+    // of birth to their year of death
+    newArr = arr.map((person) => (person.yearOfDeath - person.yearOfBirth))
+    let max = 0;
+    let position = 0;
+    // It returns 'position' which will be the position in 'arr' to hold the
+    // oldest person
+    for (let i = 0; i < arr.length; i++) {
+       if (max < newArr[i]) {
+            max = newArr[i];
+            position = i;
+       } 
+    }
+    return arr[position];
 };
 
 // Do not edit below this line
