501c251a9c0c5f29dd95259653134481598ec625
debian-mocos <130593875+MoXcz@users.noreply.github.com>
2024-11-06 18:18:23 -0500

    Add solution for sumAll-5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  let sum = 0;
+  if (
+    start < 0 ||
+    end < 0 ||
+    !Number.isInteger(start) ||
+    !Number.isInteger(end)
+  )
+    return "ERROR";
+  if (start < end) {
+    for (let i = 1; i <= end; i++) {
+      sum += i;
+    }
+  } else if (start > end) {
+    for (let i = start; i > 0; i--) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
