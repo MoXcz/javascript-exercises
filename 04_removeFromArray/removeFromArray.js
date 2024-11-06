5e03b31aebdfa5f383bf947e1b6b9c70a9b328e7
debian-mocos <130593875+MoXcz@users.noreply.github.com>
2024-11-05 20:44:40 -0500

    Add solution for removeFromArray-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...valueToRemove) {
+  if (arr == undefined) return "ERROR";
+  let j = 0;
+  while (valueToRemove[j] != undefined) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === valueToRemove[j]) {
+        arr.splice(i, 1);
+        i = 0;
+      }
+    }
+    j++;
+  }
+  return arr;
 };
+
+arr = removeFromArray([1, 2, 3, 4], 3, 2);
+console.log(arr);
 
 // Do not edit below this line
 module.exports = removeFromArray;
