5db31d216a1fba12d3c6ddefb7bf82dd7dbeeeb8
debian-mocos <130593875+MoXcz@users.noreply.github.com>
2024-11-15 15:46:35 -0500

    Add solution for calculator-8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let result = arr.reduce((sum, current) => sum + current, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let result = arr.reduce((prod, current) => prod * current, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let c = 1;
+  if (a > 0) c = a * factorial(a - 1);
+  return c;
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
