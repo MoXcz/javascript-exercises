ae2716973839bc503272407a56e0913f986db967
debian-mocos <130593875+MoXcz@users.noreply.github.com>
2024-11-15 21:11:38 -0500

    Add solution for fibonacci-10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (fibonacciNumber) {
+  if (fibonacciNumber == 0) return 0;
+  if (fibonacciNumber < 0) return "OOPS"
+  let curr = 0,
+    prev = 0,
+    next = 1;
+  for (let i = 1; i < +fibonacciNumber; i++) {
+    prev = curr;
+    curr = next;
+    next = prev + curr;
+  }
+  return next;
 };
 
 // Do not edit below this line
