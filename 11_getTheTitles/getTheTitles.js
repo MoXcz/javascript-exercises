d25279f45e1ef1c0a12be700c6d24dd2ce627fd8
debian-mocos <130593875+MoXcz@users.noreply.github.com>
2024-11-15 21:31:15 -0500

    Add solution for getTheTitles-11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (obj) {
+  let arr = [];
+  obj.forEach((book) => arr.push(book.title));
+  return arr;
 };
 
 // Do not edit below this line
