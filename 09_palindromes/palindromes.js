bd6262a0cde25556f558743e6e68ec250ad1b24e
debian-mocos <130593875+MoXcz@users.noreply.github.com>
2024-11-15 20:52:01 -0500

    Add solution for palindromes-9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let inOrder = string
+    .toLowerCase()
+    .replaceAll(" ", "")
+    .replaceAll(".", "")
+    .replaceAll(",", "")
+    .replaceAll("!", "")
+    .split("");
+  let rev = string
+    .toLowerCase()
+    .replaceAll(" ", "")
+    .replaceAll(".", "")
+    .replaceAll(",", "")
+    .replaceAll("!", "")
+    .split("")
+    .reverse();
+  if (rev.join("") != inOrder.join("")) {
+    return false;
+  }
 
+  return true;
 };
 
 // Do not edit below this line
