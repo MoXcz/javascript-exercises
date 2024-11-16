03fdc896ebbb9b731ffc8a008249ea78133ed038
debian-mocos <130593875+MoXcz@users.noreply.github.com>
2024-11-16 14:25:51 -0500

    Add solution for findTheOldest-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let oldestPerson = 0;
+  let oldestPersonAge = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (!arr[i].yearOfDeath) arr[i].yearOfDeath = new Date().getFullYear();
+    if (i == 0) oldestPersonAge = 0;
+    else oldestPersonAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+    if (oldestPersonAge < arr[i].yearOfDeath - arr[i].yearOfBirth) {
+      oldestPerson = arr[i];
+    }
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line
